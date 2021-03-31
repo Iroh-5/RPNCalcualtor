e2d9c9b7928aad78bfccee6d887e07f8cdfc7610
Iroh-5 <58617376+Iroh-5@users.noreply.github.com>
2021-03-31 22:52:45 +0300

    Add files via upload

--- source.cpp
+++ source.cpp
@@ -5,8 +5,47 @@
 #include <sstream>
 #include <cctype>
 #include <cmath>
+#include <functional>
+#include <iomanip>
+
+/*
+*	double exp(double)
+*	double log(double)
+*	double log2(double)
+*	double log10(double)
+*	double sqrt(double)
+*	double cbrt(double)
+*	double sin(double)
+*	double cos(double)
+*	double tan(double)
+*	double asin(double)
+*	double acos(double)
+*	double atan(double)
+*	double pow(double, double)
+*/
+
+[[noreturn]] void error(const std::string& message)
+{
+	std::cerr << message << std::endl;
+	std::terminate();
+}
 
 std::unordered_map<std::string, double> vars;
+std::unordered_map<std::string, std::function<double(double)>> funcs =
+{
+	{"exp", exp},
+	{"log", log},
+	{"log2", log2},
+	{"log10", log10},
+	{"sqrt", sqrt},
+	{"cbrt", cbrt},
+	{"sin", sin},
+	{"cos", cos},
+	{"tan", tan},
+	{"asin", asin},
+	{"acos", acos},
+	{"atan", atan}
+};
 
 void calculate(std::stringstream& ss)
 {
@@ -38,8 +77,7 @@ void calculate(std::stringstream& ss)
 			double op2 = s.top(); s.pop();
 			if (op1 == 0)
 			{
-				std::cerr << "Division by zero" << std::endl;
-				std::terminate();
+				error("Division by zero");
 			}
 			s.push(op2 / op1);
 		}
@@ -49,8 +87,7 @@ void calculate(std::stringstream& ss)
 			int op2 = static_cast<int>(s.top()); s.pop();
 			if (op1 == 0)
 			{
-				std::cerr << "Division by zero" << std::endl;
-				std::terminate();
+				error("Division by zero");
 			}
 			s.push(op2 % op1);
 		}
@@ -59,24 +96,45 @@ void calculate(std::stringstream& ss)
 			ss.putback(ch);
 			std::string iStr;
 			ss >> iStr;
-			if (iStr.compare("let") == 0)
+			if (iStr == "let")
 			{
 				std::string val;
 				ss >> iStr >> ch >> val;
 				vars[iStr] = atof(val.c_str());
 				return;
 			}
+			else if (iStr == "pow")
+			{
+				if (s.size() < 2)
+				{
+					std::cerr << "Function \"pow\" needs two arguments" << std::endl;
+					return;
+				}
+				double a = s.top(); s.pop();
+				double b = s.top(); s.pop();
+				s.push(pow(b, a));
+			}
+			else if (auto entity = funcs.find(iStr); entity != funcs.end())
+			{
+				if (s.size() == 0)
+				{
+					std::cout << "Function " << std::get<0>(*entity) << " needs argument" << std::endl;
+					return;
+				}
+
+				auto func = std::get<1>(*entity);
+				double a = s.top(); s.pop();
+				s.push(func(a));
+			}
 			else
 			{
 				if (vars.count(iStr) != 1)
 				{
 					std::cerr << "Undefined identifier" << std::endl;
-					std::terminate();
+					return;
 				}
-				else
-				{
-					s.push(vars[iStr]);
-				}
+
+				s.push(vars[iStr]);
 			}
 		}
 		else
@@ -91,7 +149,7 @@ void calculate(std::stringstream& ss)
 	if (s.size() > 1)
 		std::cout << "Invalid syntax" << std::endl;
 	else
-		std::cout << "= " << (!s.empty() ? s.top() : 0) << std::endl;
+		std::cout << std::setprecision(10) << "= " << (!s.empty() ? s.top() : 0) << std::endl;
 }
 
 int main()
